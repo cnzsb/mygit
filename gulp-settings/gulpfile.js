fbc3896eaf174d68f5787634a16d28ac34f7b5d5
cnzsb <cnzsb@foxmail.com>
2015-12-31 15:45:16 +0800

    update

--- gulp-settings/gulpfile.js
+++ gulp-settings/gulpfile.js
@@ -14,51 +14,58 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync');
 
 gulp.task('html', function () {
-	return gulp.src('src/*.html')
-		.pipe(gulp.dest('dist/'))
+	return gulp.src('src/**/*.html', {base: 'src'})
+		.pipe(gulp.dest('.'))
 		.pipe(notify({message: 'HTML done!'}));
 });
 
 gulp.task('sass', function () {
-	return sass('src/sass/*.scss', {style: 'expanded'})
+	return sass('src/**/*.scss', {base: 'src'}, {style: 'expanded'})
 		.pipe(autoprefixer('last 2 version', 'ie >= 7'))
-		.pipe(gulp.dest('dist/css/'))
+		.pipe(gulp.dest('.'))
 		.pipe(notify({message: 'Sass done!'}));
 });
 
+gulp.task('css', function () {
+	return gulp.src('src/**/*.css', {base: 'src'})
+		.pipe(gulp.dest('.'))
+		.pipe(notify({message: 'CSS done!'}));
+});
+
 gulp.task('js', function () {
-	return gulp.src('src/js/*.js')
+	return gulp.src('src/**/*.js', {base: 'src'})
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
-		.pipe(gulp.dest('dist/js/'))
+		.pipe(gulp.dest('.'))
 		.pipe(notify({message: 'JS done!'}));
 });
 
 gulp.task('img', function () {
-	return gulp.src('src/images/*')
+	return gulp.src('src/**/images/*', {base: 'src'})
 		.pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
-		.pipe(gulp.dest('dist/images/'))
+		.pipe(gulp.dest('.'))
 		.pipe(notify({message: 'Images done!'}))
 });
 
-gulp.task('default', ['html', 'sass', 'js', 'img']);
+gulp.task('default', ['html', 'sass', 'css', 'js', 'img']);
 
-gulp.task('refresh', ['html', 'sass', 'js', 'img'], function () {
+gulp.task('refresh', ['html', 'sass', 'css', 'js', 'img'], function () {
 	var files = [
-		'dist/*.html',
-		'dist/css/*.css',
-		'dist/js/*.js',
-		'dist/images/*'
+		'./**/*.html',
+		'./**/*.css',
+		'./**/*.js',
+		'./**/images/*'
 	];
 
 	browserSync.init(files, {
 		server: {
-			baseDir: 'dist/'
+			baseDir: '.'
 		}
 	});
 
-	gulp.watch('src/*.html', ['html']);
-	gulp.watch('src/sass/*.scss', ['sass']);
-	gulp.watch('src/js/*.js', ['js']);
-	gulp.watch('src/images/*', ['img']);
+	gulp.watch('src/**/*.html', ['html']);
+	gulp.watch('src/**/*.scss', ['sass']);
+	gulp.watch('src/**/*.css', ['css']);
+	gulp.watch('src/**/*.js', ['js']);
+	gulp.watch('src/**/images/*', ['img']);
 });
